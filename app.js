19352944fb442d807a2bd35663d37b293fe9ecee
Nikita <srivastavanikita1996@gmail.com>
2019-09-13 01:30:47 +0530

    code to covert a html code into an image

---
+++ app.js
@@ -0,0 +1,25 @@
+var puppet  = require('puppeteer');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
+app.post('/html-to-image', (req, res)=>{
+    puppet.launch().then(async browser =>{
+        htmlContent = req.body.htmldata
+        const page = await browser.newPage();
+        await page.setContent(htmlContent);
+        const image = await page.screenshot({
+            path:"image.jpeg"
+        })
+        await browser.close();
+        res.end(image);
+    })
+    //res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
